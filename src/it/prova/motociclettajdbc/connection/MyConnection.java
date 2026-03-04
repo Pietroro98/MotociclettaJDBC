4b3551fec807d0b70e781a0f84cdb885d59e6f9b
Pietro Romano <pietroro98@gmail.com>
2026-03-04 17:19:35 +0100

    aggiornamento

--- src/it/prova/motociclettajdbc/connection/MyConnection.java
+++ src/it/prova/motociclettajdbc/connection/MyConnection.java
@@ -5,17 +5,21 @@ import java.sql.DriverManager;
 
 public class MyConnection {
     private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/motociclettajdbc?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
     private static final String USER = System.getenv("DB_USER");
     private static final String PASSWORD = System.getenv("DB_PASSWORD");
-
+    private static final String URL = "jdbc:mysql://localhost:3306/motociclettajdbc"
+            + "?user=" + USER
+            + "&password=" + PASSWORD
+            + "&allowPublicKeyRetrieval=true"
+            + "&useSSL=false"
+            + "&serverTimezone=UTC";
     public static Connection getConnection()
     {
         Connection connection = null;
         try
         {
             Class.forName(DRIVER_NAME);
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(URL);
 
         } catch (Exception ex) {
 
